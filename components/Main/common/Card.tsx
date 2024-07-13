1f693ddc2c456e6e2892e377a40aaab2feb1599b
cw <fromnowwon@gmail.com>
2024-07-14 00:58:55 +0900

    feat: 무비차트, 상영예정작 출력, swiper 적용

--- components/Main/common/Card.tsx
+++ components/Main/common/Card.tsx
@@ -1,3 +1,43 @@
-export default function Card() {
-  return <div>Card</div>;
+import Image from "next/image";
+import Link from "next/link";
+import { FiThumbsUp } from "react-icons/fi";
+import { Movie } from "@/types/MovieTypes";
+
+interface CardProps {
+  movie: Movie;
+}
+
+export default function Card({ movie }: CardProps) {
+  return (
+    <div className="group w-[170px] cursor-pointer sm:hover:shadow-slate-300 sm:shadow-md sm:border sm:border-slate-200 sm:m-2 transition-shadow duration-200">
+      <Link href={`detail/${movie.id}`}>
+        <div className="relative w-full h-[234px] overflow-hidden">
+          <Image
+            src={`https://image.tmdb.org/t/p/original${
+              movie.poster_path || movie.backdrop_path
+            }
+      }`}
+            alt={movie.title ?? movie.name ?? "movie image"}
+            width={300}
+            height={500}
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mO8XA8AAisBVHpjGM0AAAAASUVORK5CYII="
+            className="absolute inset-0 w-full h-full object-cover"
+          ></Image>
+        </div>
+        <div className="w-full p-2">
+          <h2 className="text-md font-bold truncate">
+            {movie.title || movie.name}
+          </h2>
+          <p className="flex items-center">
+            <span className="text-sm">
+              {movie.release_date || movie.first_air_date}
+            </span>
+            <FiThumbsUp className="h-5 mr-1 ml-3 size-3" />
+            <span className="text-sm">{movie.vote_count}</span>
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
 }
--- components/Main/common/Results.tsx
+++ components/Main/common/Results.tsx
@@ -1,9 +1,45 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./SwiperCustom.css";
 import { Movie } from "@/types/MovieTypes";
+import Card from "./Card";
 
 interface ResultsProps {
   movies: Movie[];
 }
 
 export default function Results({ movies }: ResultsProps) {
-  return <div>Results</div>;
+  return (
+    <div className="mt-3 min-h-[294px]">
+      <Swiper
+        modules={[Navigation, Scrollbar, A11y]}
+        spaceBetween={30}
+        slidesPerView={1}
+        breakpoints={{
+          390: {
+            slidesPerView: 2,
+          },
+          640: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 5,
+          },
+        }}
+        navigation
+        scrollbar={{ draggable: true }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {movies &&
+          movies.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <Card movie={movie} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
+  );
 }
